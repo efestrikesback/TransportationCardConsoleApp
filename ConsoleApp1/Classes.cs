03ee6d13a8e2790a5a03a642703bf9cc3941166f
efestrikesback <96078086+efestrikesback@users.noreply.github.com>
2022-03-17 14:06:11 +0300

    uploaded version 1.0

---
+++ ConsoleApp1/Classes.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class PersonalDetails
+    {
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public string IdNumber { get; set; }
+        public String DiscountType { get; set; }
+    }
+
+    public class Card
+    {
+        private double fund;
+        public string cardNumber;
+        private readonly PersonalDetails personalDetails;
+        private bool physical;
+
+        public Card()
+        {
+            this.physical = true;
+            Initialize();
+        }
+
+        public Card(PersonalDetails personalDetails, bool physical)
+        {
+            Initialize();
+            this.personalDetails = personalDetails;
+            this.physical = physical;
+        }
+
+        private void Initialize()
+        {
+            Random random = new Random();
+            for (int i = 0; i <= 4; i++)
+            {
+                this.cardNumber += random.Next(1000, 9999);
+            }
+
+            this.fund = 0;
+        }
+
+        public void Deposit(double amount)
+        {
+            if (amount <= 0)
+            {
+                throw new Exception("You must deposit a positive amount");
+            }
+
+            this.fund += amount;
+        }
+        public void Charge(double amount)
+        {
+            if (amount <= 0)
+            {
+                throw new Exception("You must charge a positive amount");
+            }
+
+            double discountedAmount = ApplyDiscount(amount);
+
+            if (this.fund - discountedAmount < 0)
+            {
+                throw new Exception("You do not have enough funds to make this payment");
+            }
+
+            this.fund -= discountedAmount;
+        }
+        private double ApplyDiscount(double applyTo)
+        {
+            if (this.personalDetails == null)
+            {
+                return applyTo;
+            }
+
+            return applyTo * (double)dictionary[this.personalDetails.DiscountType];
+        }
+
+        private readonly Dictionary<string, double> dictionary = new Dictionary<string, double>()
+        {
+            {"Student", 0.25},
+            {"PublicServant", 0.75},
+            {"Senior", 0.4},
+            {"None", 1},
+        };
+
+        public override string ToString()
+        {
+            string result = $"Card number: {this.cardNumber}" + Environment.NewLine;
+            result = result + $"Funds: {this.fund}" + Environment.NewLine;
+
+            string type = this.physical ? "physical" : "digital";
+            result = result + $"Type: {type}" + Environment.NewLine;
+
+            if (personalDetails != null)
+            {
+                result = result + $"Registered to" + Environment.NewLine;
+                result = result + $"Id :{this.personalDetails.IdNumber}" + Environment.NewLine;
+
+                result = result + $"Name: {this.personalDetails.Name}" + Environment.NewLine;
+                result = result + $"Surname: {this.personalDetails.SurName}" + Environment.NewLine;
+                result = result + $"Discount type: {this.personalDetails.DiscountType}" + Environment.NewLine;
+            }
+
+            return result;
+        }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,152 @@
+﻿
+using Classes;
+class Program
+{
+    private static Card card;
+    static void Main(string[] args)
+    {
+        card = CardWizard.CreateCard();
+        while (true)
+        {
+            PrintMainMenu();
+        }
+
+    }
+
+    static void PrintMainMenu()
+    {
+
+        Console.WriteLine("Select and option.");
+        Console.WriteLine("1 Deposit funds.");
+        Console.WriteLine("2 Take a ride.");
+        Console.WriteLine("3 Display account.");
+        Console.WriteLine("4 Exit program.");
+
+        int selectedOption = Convert.ToInt32(Console.ReadLine());
+
+        if (selectedOption == 1)
+        {
+            Console.WriteLine("Enter amount to deposit.");
+            card.Deposit(double.Parse(Console.ReadLine()));
+        }
+
+        if (selectedOption == 2)
+        {
+            Console.WriteLine("Enter ticket price.");
+            try
+            {
+                double amount = double.Parse(Console.ReadLine());
+                card.Charge(amount);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        if (selectedOption == 3)
+        {
+            Console.WriteLine(card.ToString());
+        }
+
+        if (selectedOption == 4)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
+
+#region  Gandalf Selection Wizard 
+public class CardWizard
+{
+    public static Card CreateCard()
+    {
+        Console.WriteLine("Let's create your card...");
+
+        Console.WriteLine("Select card type");
+        Console.WriteLine("1 Digital");
+        Console.WriteLine("2 Physical");
+        Console.WriteLine("3 Physical anonymous");
+
+        int option = Convert.ToInt32(Console.ReadLine());
+
+        switch (option)
+        {
+            case 1:
+                return DigitalCardWizard();
+            case 2:
+                return PhysicalCardWizard();
+            case 3:
+                return new Card();
+            default: throw new Exception("You can only choose 1, 2 or 3 ");
+        }
+    }
+
+    private static Card PhysicalCardWizard()
+    {
+        Console.WriteLine("Would you like to register your card?");
+        Console.WriteLine("1 Yes");
+        Console.WriteLine("2 No");
+
+        if (Convert.ToInt32(Console.ReadLine()) == 2)
+        {
+            return new Card();
+        }
+
+        return new Card(PersonalDetailsWizard(), true);
+
+    }
+
+    private static Card DigitalCardWizard()
+    {
+        return new Card(PersonalDetailsWizard(), false);
+    }
+
+    private static PersonalDetails PersonalDetailsWizard()
+    {
+        Console.WriteLine("Enter id number");
+        string idNumber = Console.ReadLine();
+
+        Console.WriteLine("Enter name");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("Enter surname");
+        string surname = Console.ReadLine();
+
+
+        Console.WriteLine("Select discount type");
+        Console.WriteLine("1 Student");
+        Console.WriteLine("2 Senior");
+        Console.WriteLine("3 Public servant");
+        Console.WriteLine("4 No discount");
+
+        int discountType = Convert.ToInt32(Console.ReadLine());
+
+        PersonalDetails personalDetails = new PersonalDetails()
+        {
+            IdNumber = idNumber,
+            Name = name,
+            SurName = surname
+        };
+
+        switch (discountType)
+        {
+            case 1:
+                personalDetails.DiscountType = "Student";
+                break;
+            case 2:
+                personalDetails.DiscountType = "Senior";
+                break;
+            case 3:
+                personalDetails.DiscountType = "Public servant";
+                break;
+            default:
+                personalDetails.DiscountType = "None";
+                break;
+        }
+
+        return personalDetails;
+    }
+}
+
+#endregion 
